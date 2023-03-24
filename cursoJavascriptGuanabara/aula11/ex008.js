5a6f10934c7801b095a102c1462f47ea0e0837c2
Mambo <nelson-carvalho@live.com>
2023-03-24 00:56:15 -0300

    aprendizadoEmJavascript

---
+++ cursoJavascriptGuanabara/aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('O Console funcionou!')
+
+var vel = 72.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ // Condição simples, porque não tem else.
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
