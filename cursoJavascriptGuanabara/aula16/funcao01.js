92f83998055761bb58a134f5ddd9cb715bdb32d6
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-28 18:07:06 -0300

    update

---
+++ cursoJavascriptGuanabara/aula16/funcao01.js
@@ -0,0 +1,25 @@
+/*
+//Função + parametro
+function ação(){
+
+    //Ação
+
+    //Retorno de um resultado
+    return res
+}
+
+//Chamada para função
+ação(5)
+*/
+
+function parimpar(n){
+    if (n%2 == 0) {
+        return "Par!"
+    } else {
+        return "Ímpar!"
+    }
+}
+
+console.log(parimpar(223))
+//let res = parimpar(4)
+//console.log(res)
