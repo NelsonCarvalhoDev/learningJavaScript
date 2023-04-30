99c00204413f662fb6344883950e26ecbc06b09b
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-30 16:31:07 -0300

    aula03

---
+++ cursoJSMatheusBattistiYouTube/3_particularidades_JS/js/script.js
@@ -0,0 +1,16 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+/* 
+Case sensitive, não interpreta erros.
+console.LOG("")
+*/
+
+// Variavel pode ser alterada ao longo do caminho, tipagem fraca.
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
