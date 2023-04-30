eee25390f871896c8d2f2603fcbc50e4490151c6
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-30 20:55:18 -0300

    update_folder

---
+++ cursoJSMatheusBattistiYouTube/5_number/script.js
@@ -0,0 +1,12 @@
+/*
+Tipos de dados
+
+No JavaScript temos diversos tipos de dados, os mais utilizados são:
+Number, String, Boolean, null e undefined, Object;
+Podemos verificar o tipo do dado com o operador: typeof;
+
+Number:
+O Number é o tipo de dado para números;
+Possui tês valores simbólicos: +Infinity, -Infinity e NaN;
+Não existe um tipo definido para inteiros, todo número é um Number;
+*/
