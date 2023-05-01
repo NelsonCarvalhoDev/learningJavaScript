51be0f30041211dd91769515ab79b45b6c268b59
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-30 22:18:37 -0300

    aula8_undefined_null

---
+++ cursoJSMatheusBattistiYouTube/8_undefined_e_null/js/script.js
@@ -0,0 +1,30 @@
+/*
+
+palavras reservada da linguagem
+
+undefined e null:
+
+undefined e null também são considerados tipos de dados;
+O null é um tipo de dado que representa um valor;
+O undefined é um tipo de dado para variável com valor não atribuído;
+
+console.log('undefined e null');
+
+*/
+console.log('undefined e null: ');
+
+// Hoisting - içamento
+console.log(sobreNome);
+console.log(numero);
+
+var nome = null;
+var sobreNome = 'Carvalho';
+
+console.log(nome);
+console.log(sobreNome);
+
+nome = 'nelson'
+
+console.log(nome);
+
+var numero = 5;
