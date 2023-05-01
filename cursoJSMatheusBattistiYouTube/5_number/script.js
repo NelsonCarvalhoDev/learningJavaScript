3a84d909888142b6dc39d6b021bc77d63feb76b2
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-30 21:04:17 -0300

    aula6_number

--- cursoJSMatheusBattistiYouTube/5_number/script.js
+++ cursoJSMatheusBattistiYouTube/5_number/script.js
@@ -10,3 +10,29 @@ O Number é o tipo de dado para números;
 Possui tês valores simbólicos: +Infinity, -Infinity e NaN;
 Não existe um tipo definido para inteiros, todo número é um Number;
 */
+
+// typeof
+let numero = 7;
+
+console.log(numero);
+console.log(typeof numero);
+
+//Float é considerado um Number
+let float = 5.32;
+console.log(float);
+console.log(typeof float);
+
+//Não é considerado Number por estar em String
+let textoComNumero = '423';
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+
+//Number
+console.log(NaN);
+console.log(typeof NaN);
+console.log(+Infinity);
+console.log(typeof +Infinity);
+console.log(-Infinity);
+console.log(typeof -Infinity);
