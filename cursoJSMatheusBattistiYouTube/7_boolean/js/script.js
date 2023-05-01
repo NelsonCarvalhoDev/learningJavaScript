f09a3687ddb3f12e21b12add4cea299072d5a481
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-04-30 22:05:52 -0300

    aula7_boolean

---
+++ cursoJSMatheusBattistiYouTube/7_boolean/js/script.js
@@ -0,0 +1,28 @@
+/*
+Boolean:
+
+O boolean representa um valor verdadeiro ou falso;
+Comparações resultam em booleans;
+Podemos também atribuir um true ou false para uma varável e ela terá o tipo boolean;
+*/
+console.log('Boolean');
+
+let verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+console.log(typeof true);
+console.log(typeof false);
+
+let falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+/*
+
+Variáveis reservadas:
+
+var, true, false, function e etc...
+
+*/
