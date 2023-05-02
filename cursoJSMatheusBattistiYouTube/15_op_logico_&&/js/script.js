02595dc24657e133eb5ae5d47adce784d120a3b2
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-05-02 10:32:38 -0300

    aula15_op_AND_&&

---
+++ cursoJSMatheusBattistiYouTube/15_op_logico_&&/js/script.js
@@ -0,0 +1,36 @@
+/*
+Operadores Lógicos:
+
+Nas linguagens doe programação existem os operadores lógicos;
+Estes operadores realizam comparações para que seja retornado um true ou false;
+Decidindo então o fluxo da aplicação;
+Utilizamos prncipalmente nbas unstruçõesde condição e repetição, em conjunto dos operadores de comparação;
+
+
+Operador lógico &&
+
+O operador lógico && é conhecido também como AND;
+Ele vai retornar true apenas se as duas expressões retornarem true;
+Qualquer outro resultado o operador lógico and retornará false;
+*/
+
+console.log('Operadores Lógicos: AND ou &&')
+
+var idade = 27;
+var nome = 'Nelson';
+
+if(nome == 'Nelson' && idade == 27){ // O operador && verifica se da match(true) em ambos os lados.
+    console.log('O Nelson pode entrar na aula!');
+} else {
+    console.log('Este não é o Nelson!')
+}
+
+
+if(1 == 1 && 3 > 2 && true){ // Pode se encadear quantas expressões necessária.
+    console.log('Passou')
+}
+
+//Isolar operações
+if((1 == 1 && 3 > 2) && true){ // Pode se isolar com parenteses() a operação ser resolvida primeiro.
+    console.log('Passou')
+}
