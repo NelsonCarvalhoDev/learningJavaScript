ba0c3d8bd4ae06f101d8bb55634b708ba2ada99a
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-05-08 10:15:15 -0300

    aula33_acessando_DOM

--- cursoJSMatheusBattistiYouTube/33_DOM/js/script.js
+++ cursoJSMatheusBattistiYouTube/33_DOM/js/script.js
@@ -15,3 +15,23 @@ Acessar o DOM é semelhante as regras de CSS;
 Podemos acessar por: tags, ids, classes;
 */
 console.log('Document object model: ')
+
+//Acessando elementos por tag:
+
+var titulo = document.getElementsByTagName('h1')[0];
+console.log(titulo);
+
+var lis = document.getElementsByTagName('li');
+console.log(lis[3]);
+
+// Id :
+
+var paragrafo = document.getElementById('paragrafo');
+console.log(paragrafo);
+
+
+// class
+
+ var itensDaLista = document.getElementsByClassName('item');
+ console.log(itensDaLista);
+ 
