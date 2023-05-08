fc74c9aec9d0a7df51615a25216469c4f79a2c51
Mambo <109152434+MamboDark@users.noreply.github.com>
2023-05-08 10:04:13 -0300

    update

--- cursoJSMatheusBattistiYouTube/34_DOM/js/script.js
+++ cursoJSMatheusBattistiYouTube/34_DOM/js/script.js
@@ -1,11 +1,17 @@
 /*
-Document object model:
+Document object model: Intrudoção
 
 Uma interface de programação para HTML
 Por meio dele temos método para acessar a árvore de elementos;
 O DOM fornece uma cópia do HTML;
 Podemos manupular eventos pelo DOM para afetar o HTML;
-
 document = Pai de todos, tudo pode ser acessado por ele.
+
+Document object model:
+
+Acessar o DOM caracteriza-se por identrificar um elemento do HTML através de métodos;
+Depois podemos manupulá-los da forma que quisermos;
+Acessar o DOM é semelhante as regras de CSS;
+Podemos acessar por: tags, ids, classes;
 */
 console.log('Document object model: ')
---
+++ cursoJSMatheusBattistiYouTube/DOM/js/script.js
@@ -0,0 +1,11 @@
+/*
+Document object model: Intrudoção
+
+Uma interface de programação para HTML
+Por meio dele temos método para acessar a árvore de elementos;
+O DOM fornece uma cópia do HTML;
+Podemos manupular eventos pelo DOM para afetar o HTML;
+
+document = Pai de todos, tudo pode ser acessado por ele.
+*/
+console.log('Document object model: ')
